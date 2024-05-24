7ca8e043c6131dce42225398d50b7ff6244f5b01
shri harini <shri13anitha@gmail.com>
2024-05-24 20:41:15 +0530

    modified back

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-/*import Image from "next/image";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -7,15 +7,3 @@ export default function Home() {
     </main>
   );
 }
-*/
-const number1 = document.getElementById("num1") as HTMLInputElement
-const printButton = document.getElementById("printBtn") as HTMLButtonElement
-const printValue = document.getElementById("enteredNumber") as HTMLOutputElement
-
-function printEnteredValue(): void{
-    const numb1 = parseFloat(number1.value);
-    printValue.textContent = numb1.toString();
-
-}
-
-printButton.addEventListener("click", printEnteredValue);
