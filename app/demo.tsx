e573e1bc1ab1b8c8c1f8349bdd362a5305296e16
shriharini10 <shri13anitha@gmail.com>
2024-05-26 23:46:17 +0530

    feat: add new tsx file

---
+++ app/demo.tsx
@@ -0,0 +1,19 @@
+interface detail{
+    name:string;
+    rollno:number;
+    id:number;
+    city?:string;
+    printst():void;
+}
+
+const stud:detail{
+    name:"harini",
+    rollno:02,
+    id:3714,
+    printst(){
+        console.log('${this.name} ${this.rollno}');
+};
+
+console.log("Student information:");
+stud.printst();
+console.log(stud.id);
